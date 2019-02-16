2d587581b3d5c837e01af2e26cf4d94e8af7c5bb
dohuuvi <mrviit@gmail.com>
2019-02-16 11:19:46 +0700

    I can create the `AAA` instance now.

--- tests/hello.test.ts
+++ tests/hello.test.ts
@@ -12,17 +12,24 @@ describe("Hello Sinon", () => {
     let a: any;
     beforeEach(() => {
 
+        
+        // var stub = sinon.stub().throws();
         // How to isolate new AAA from BBB?
-        a = new AA();  // error because of BBB::constructor throws an error
+        // a = new AA();  // error because of BBB::constructor throws an error
     });
 
     describe("#hello", () => {
         it("#hello", async () => {
+            const stub = sinon.stub().callsFake(() => { });
+            Object.setPrototypeOf(AA, stub);
+            a = new AA();
+
             sinon.spy(a, "hello");
 
             a.hello();
 
             sinon.assert.calledOnce(a.hello);
+            sinon.assert.calledOnce(stub);
 
             // how to verify that super.say has been called once with string "Hello!"?
         });
